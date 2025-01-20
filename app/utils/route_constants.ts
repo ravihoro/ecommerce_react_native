0990bfda1cf170ce2871b21f9ef91b8fa0a98cd3
ravihoro <ravijohnhoro5@gmail.com>
2025-01-20 18:04:30 +0530

    added route and theme utilities

---
+++ app/utils/route_constants.ts
@@ -0,0 +1,10 @@
+export const Routes = {
+  HOME: "home" as const,
+  CART: "cart" as const,
+  NOTIFICATION: "notification" as const,
+  PROFILE: "profile" as const,
+  HOME_BOTTOM_TAB: "home_bottom_tab" as const,
+  NOTIFICATION_BOTTOM_TAB: "notification_bottom_tab" as const,
+  CART_BOTTOM_TAB: "cart_bottom_tab" as const,
+  PROFILE_BOTTOM_TAB: "profile_bottom_tab" as const,
+};
---
+++ app/utils/theme.ts
@@ -0,0 +1,29 @@
+const Palette = {
+  lightPurple: "#e8e1fc",
+  purple: "#8e6cf0",
+  grey: "#f4f4f4",
+  darkGrey: "#272727",
+  white: "#ffffff",
+};
+
+export const Theme = {
+  colors: {
+    primary: Palette.purple,
+    primaryLight: Palette.lightPurple,
+    background: Palette.white,
+    greyBackground: Palette.grey,
+  },
+  fontSize: {
+    xs: 8,
+    s: 12,
+    m: 16,
+    l: 20,
+    xl: 24,
+  },
+  spacing: {
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
+  },
+};
